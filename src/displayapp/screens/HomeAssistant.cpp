af167feece5c3361064aea17c1601577493d335b
Rongen, Jeffrey <j.rongen@nfi.nl>
2024-09-18 22:55:51 +0200

    Start work on HomeAssistant screen for communicating with BLE service

---
+++ src/displayapp/screens/HomeAssistant.cpp
@@ -0,0 +1,70 @@
+#include "displayapp/screens/HomeAssistant.h"
+#include <cstdint>
+#include "displayapp/DisplayApp.h"
+#include "components/ble/HomeAssistantService.h"
+
+using namespace Pinetime::Applications::Screens;
+
+static void event_handler(lv_obj_t* obj, lv_event_t event) {
+  HomeAssistant* screen = static_cast<HomeAssistant*>(obj->user_data);
+  screen->OnObjectEvent(obj, event);
+}
+
+HomeAssistant::HomeAssistant(Pinetime::Controllers::HomeAssistantService& home) : homeAssistantService(home) {
+  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_static(title, "Home Assistant");
+  lv_obj_align(title, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 0, 0);
+  lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
+  
+  SwtStatus1 = lv_switch_create(lv_scr_act(), nullptr);
+  SwtStatus1->user_data = this;
+  lv_obj_set_size(SwtStatus1, 80, 40);
+  lv_obj_align(SwtStatus1, lv_scr_act(), LV_ALIGN_IN_TOP_LEFT, 5, 35);
+  LblStatus1 = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_static(LblStatus1, "---");
+  lv_obj_align(LblStatus1, SwtStatus1, LV_ALIGN_OUT_RIGHT_MID, 25, 0); 
+  lv_obj_set_event_cb(SwtStatus1, event_handler);
+
+  SwtStatus2 = lv_switch_create(lv_scr_act(), nullptr);
+  SwtStatus2->user_data = this;
+  lv_obj_set_size(SwtStatus2, 80, 40);
+  lv_obj_align(SwtStatus2, lv_scr_act(), LV_ALIGN_IN_TOP_LEFT, 5, 85);
+  LblStatus2 = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_fmt(LblStatus2, "---");
+  lv_obj_align(LblStatus2, SwtStatus2, LV_ALIGN_OUT_RIGHT_MID, 25, 0); 
+  lv_obj_set_event_cb(SwtStatus2, event_handler);
+
+  SwtStatus3 = lv_switch_create(lv_scr_act(), nullptr);
+  SwtStatus3->user_data = this;
+  lv_obj_set_size(SwtStatus3, 80, 40);
+  lv_obj_align(SwtStatus3, lv_scr_act(), LV_ALIGN_IN_TOP_LEFT, 5, 135);
+  LblStatus3 = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_fmt(LblStatus3, "---");
+  lv_obj_align(LblStatus3, SwtStatus3, LV_ALIGN_OUT_RIGHT_MID, 25, 0); 
+  lv_obj_set_event_cb(SwtStatus3, event_handler);
+
+  SwtStatus4 = lv_switch_create(lv_scr_act(), nullptr);
+  SwtStatus4->user_data = this;
+  lv_obj_set_size(SwtStatus4, 80, 40);
+  lv_obj_align(SwtStatus4, lv_scr_act(), LV_ALIGN_IN_TOP_LEFT, 5, 185);
+  LblStatus4 = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_fmt(LblStatus4, "---");
+  lv_obj_align(LblStatus4, SwtStatus4, LV_ALIGN_OUT_RIGHT_MID, 25, 0); 
+  lv_obj_set_event_cb(SwtStatus4, event_handler);
+
+  
+}
+
+HomeAssistant::~HomeAssistant() {
+  lv_obj_clean(lv_scr_act());
+}
+
+void HomeAssistant::OnObjectEvent(lv_obj_t* obj, lv_event_t event) {
+  if (event == LV_EVENT_CLICKED) {
+    if (obj == SwtStatus1) {
+      lv_label_set_text_fmt(LblStatus1, "Status: %d", lv_switch_get_state(SwtStatus1));
+      homeAssistantService.setStatus(lv_switch_get_state(SwtStatus1));
+      // homeAssistantService.event((char)lv_switch_get_state(swtStatus));
+    }
+  }
+}
---
+++ src/displayapp/screens/HomeAssistant.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "displayapp/apps/Apps.h"
+#include "displayapp/screens/Screen.h"
+#include "displayapp/Controllers.h"
+#include "Symbols.h"
+
+namespace Pinetime {
+  namespace Controllers {
+    class HomeAssistantService;
+  }
+
+  namespace Applications {
+    namespace Screens {
+      class HomeAssistant : public Screen {
+      public:
+        HomeAssistant(Pinetime::Controllers::HomeAssistantService& home);
+        
+        ~HomeAssistant() override;
+
+        void OnObjectEvent(lv_obj_t* obj, lv_event_t event);
+      
+      private:
+        lv_obj_t* SwtStatus1;
+        lv_obj_t* LblStatus1;
+
+        lv_obj_t* SwtStatus2;
+        lv_obj_t* LblStatus2;
+
+        lv_obj_t* SwtStatus3;
+        lv_obj_t* LblStatus3;
+
+        lv_obj_t* SwtStatus4;
+        lv_obj_t* LblStatus4;
+
+        Pinetime::Controllers::HomeAssistantService& homeAssistantService;
+
+      };
+    }
+    
+    template <>
+    struct AppTraits<Apps::HomeAssistant> {
+      static constexpr Apps app = Apps::HomeAssistant;
+      static constexpr const char* icon = Screens::Symbols::home; 
+      
+      static Screens::Screen* Create(AppControllers& controllers) {
+        return new Screens::HomeAssistant(*controllers.homeAssistantService);
+      }
+    };
+  }
+}
